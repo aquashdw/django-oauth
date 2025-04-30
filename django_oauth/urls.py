00c023700bfa1d764a4f9507c7d79767e18c51a0
aquashdw <aquashdw@gmail.com>
2025-04-30 16:34:13 +0900

    redirect / to accounts:index

--- django_oauth/urls.py
+++ django_oauth/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('', lambda _: redirect('accounts:index')),
 ]
