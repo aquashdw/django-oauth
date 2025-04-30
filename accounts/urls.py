268ff4b9c8d8beb970ad7bf4c145aa06bf39511e
aquashdw <aquashdw@gmail.com>
2025-04-30 16:51:29 +0900

    signup view

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from accounts import views
 app_name = 'accounts'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
     # path('signin/', views.signin, name='signin'),
-    # path('signup/', views.signup, name='signup'),
     # path('profile/<username>/', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from urllib.parse import urlencode
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from accounts.forms import CustomUserCreationForm
 
 
 def index(request):
     return render(request, 'accounts/index.html')
+
+
+def signup(request):
+    # return redirect(f'{base_url}?{params}')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index', success='true')
+        
+        base_url = reverse('accounts:signup')
+        params = urlencode({'errors': form.error_messages})
+        return redirect(f'{base_url}?{params}')
+    
+    form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
