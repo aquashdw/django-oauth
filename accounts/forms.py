d7d0f25e7659bec3ed2297be9b80591936cc8a8d
aquashdw <aquashdw@gmail.com>
2025-05-02 15:26:30 +0900

    use email as username & proper validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,20 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ValidationError
 
-
+User = get_user_model()
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = get_user_model()
+    error_messages = {
+        **UserCreationForm.error_messages,
+        'duplicate_email': 'Email in use',
+    }
 
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email',)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email__iexact=email).exists():
+            raise ValidationError(self.error_messages['duplicate_email'], code='duplicate_email')
+        return email
--- accounts/views.py
+++ accounts/views.py
@@ -16,18 +16,32 @@ def signup(request):
         if form.is_valid():
             instance = form.save(commit=False)
             instance.is_active = False
-            instance.save()
+            # instance.save()
             return redirect(f'{reverse("accounts:index")}?{urlencode({"success": "true"})}')
         
         base_url = reverse('accounts:signup')
-        errors = {
-            'errors': ','.join(form.error_messages.keys())
+        errors = []
+        error_data = form.errors.as_data()
+        for error_param in error_data:
+            param_errors = error_data[error_param]
+            for error in param_errors:
+                errors.append(error.code)         
+
+        errors = ','.join(errors)
+        email = form.data.get('email')
+        params = {
+            'errors': errors,
+            'email': email,
         }
-        params = urlencode(errors)
+        params = urlencode(params)
         return redirect(f'{base_url}?{params}')
     
     form = CustomUserCreationForm()
     context = {
         'form': form,
+        'errors': request.GET.get('errors', '').split(','),
+        'email': request.GET.get('email', None),
     }
+    print(context['errors'])
+    
     return render(request, 'accounts/signup.html', context)
