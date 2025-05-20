561d49e73fa716ca101eca096524b8aaa5ecdf2e
aquashdw <aquashdw@gmail.com>
2025-05-20 09:46:36 +0900

    create client

--- oauth/views.py
+++ oauth/views.py
@@ -1,8 +1,10 @@
+from uuid import uuid4
+
 from django.contrib.auth.decorators import permission_required, login_required
 from django.shortcuts import render, redirect
-from django.views.decorators.http import require_POST
 
-from django_oauth.utils import get_perm
+from django_oauth.utils import get_perm, redirect_with_nq, extract_form_errors
+from oauth.forms import OAuthClientForm
 
 OAUTH_CODENAME = 'oauth_active'
 OAUTH_LOOKUP_NAME = 'oauth.oauth_active'
@@ -27,9 +29,28 @@ def index(request):
     return render(request, 'oauth/index.html')
 
 
-# create client
+@permission_required(OAUTH_LOOKUP_NAME, login_url='oauth:activate')
 def create_client(request):
-    pass
+    if request.method == 'POST':
+        form = OAuthClientForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.owner = request.user
+            instance.client_id = str(uuid4()).replace('-', '')
+            instance.client_secret = str(uuid4()).replace('-', '')
+            instance.save()
+            return redirect_with_nq('oauth:index', {'created': 'true'})
+
+        params = {
+            'errors': ','.join(extract_form_errors(form)),
+        }
+        return redirect_with_nq('oauth:create', params)
+
+    context = {
+        'errors': request.GET.get('errors', '').split(','),
+    }
+
+    return render(request, 'oauth/create.html', context)
 
 
 # read one client
